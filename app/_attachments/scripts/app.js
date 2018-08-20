3caac14af8bca63dc482cf5ee2a6da702d003ba7
LucasBer <lucas.berghs@gmail.com>
2018-08-20 16:42:18 +0200

    Finalizing
    Verschoning code, bugfixes en uitleg bij geschreven code.

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -11,6 +11,7 @@ myApp.config(function ($routeProvider){
 	
 });
 
+//DEZE SERVICE SAVED EN HAALT BESTANDEN VAN COUCHDB (SPECIFIEK UIT DATABASE pokemonjson)
 myApp.service("couchSrv", function($http, $q){
 	this.setObject = function(key, value){
 		$http.put('../../' + key, value);
@@ -26,9 +27,6 @@ myApp.service("couchSrv", function($http, $q){
 			for (var i = 0; i < data.data.pokemon.docs.length; i++){
 				pokemons.push(data.data.pokemon.docs[i]);
 			}
-			console.log("POKEMONS");
-			console.log(pokemons);
-			console.log(pokemons[0].name);
 			q.resolve(pokemons);
 		}, function(err){ q.reject(err);
 		});
@@ -36,6 +34,9 @@ myApp.service("couchSrv", function($http, $q){
 	};
 });
 
+//DEZE SERVICE CHECKT OF DE POKEMONS AL NIET BESTAAN, ANDERS ZOU MEN HEEL DE TIJD ERRORS KRIJGEN
+//AANGEZIEN MEN EEN OBJECT PROBEERT AAN TE MAKEN MET EEN ID DAT AL BESTAAT.
+//NIEUWE POKEMONS ZULLEN TOEGEVOEGD WORDEN. DE REEDS BESTAANDE WORDEN GENEGEERD. 
 myApp.service('checkSrv', function($http, $q){
 	this.CheckPokemon = function (){
 		var q = $q.defer();
@@ -45,23 +46,20 @@ myApp.service('checkSrv', function($http, $q){
 			for (var i = 0; i < data.data.rows.length - 1; i++){
 				pokemonids.push(data.data.rows[i].id) 
 			}
-			console.log("POKEMON IDS FOUND");
-			console.log(pokemonids);
 			q.resolve(pokemonids);
 		}, function(err){ q.reject(err); });
 		return q.promise;
 	};
 });
 
+//DEZE SERVICE ZOEKT A.D.H.V. DE VIEW ALLE RESULTATEN OP TUSSEN 2 DATUMS 
 myApp.service('viewSrv', function($http, $q){
 	this.viewSearch = function(key, key2){
 		var q = $q.defer();
 		var searchResult = [];
 		var url = "127.0.0.1:5984/pokemon2/_design/app/_view/byname?";
 		var keyurl = url + 'startkey="' + key + '"&endkey="' + key2 + '"';
-		console.log(keyurl)
 		$http.get(url).then(function(data){
-			console.log(data);
 			for (var i = 0; i < data.data.rows.length; i++){
 				searchResult.push(data.data.rows[i]);
 			}
@@ -72,10 +70,10 @@ myApp.service('viewSrv', function($http, $q){
 });
 
 myApp.controller('homeCtrl', ['$scope', 'couchSrv', 'checkSrv', 'viewSrv', function($scope, couchSrv, checkSrv, viewSrv){
+	//OPHALEN POKEMONS BIJ LADEN PAGINA
 	$( document ).ready(function() {
 		var pokemonIds = [];
 		checkSrv.CheckPokemon().then(function(data){
-			console.log("IN CHECKPOKEMON");
 			pokemonIds = data;
 		}, function(err){ console.log(err);})
 		couchSrv.getObject().then(function(data){
@@ -89,12 +87,11 @@ myApp.controller('homeCtrl', ['$scope', 'couchSrv', 'checkSrv', 'viewSrv', funct
 		});
 	});
 	
+	//ZOEKEN OP DATUMS
 	$('#searchButton').on('click', function(e){
-		console.log("Clicked!");
 		var startDate = $("#firstDate").val();
 		var endDate = $("#secondDate").val();
 		viewSrv.viewSearch(startDate, endDate).then(function(data){
-			console.log(data);
 			$scope.result = data;
 		}, function(err){ console.log(err); })
 	})
